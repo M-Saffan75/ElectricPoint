a95a1dc57f12fc71b677dbde4272c43ca82df851
M-Saffan75 <saffan.khan175@gmail.com>
2023-06-23 14:46:45 -0700

    second-commit

--- App.js
+++ App.js
@@ -4,9 +4,25 @@ import ElectricPoint from './Screens/ElectricPoint'
 const App = () => {
   return (
     <>
-      <ElectricPoint />
+      {/* <ElectricPoint /> */}
+      <View>
+        <Text>ElectricPoint</Text>
+      </View>
     </>
   )
 }
 
 export default App
+
+// git init
+// git status
+// git add .
+// git commit -m "first commit"
+// 1st time you have to manually publish
+// git push
+
+// after editing
+
+// git add filename(App.js)
+// git commit -m "message"
+// git push
